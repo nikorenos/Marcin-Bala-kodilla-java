b8e8addce45b24cc196ca27fd09bbc14e98e9472
Sentarion <mbalabox@gmail.com>
2020-06-11 12:10:32 +0200

    test update

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,47 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.*;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        int a = 10;
+        int b = 5;
+
+        Calculator add1 = new Calculator(a,b);
+
+        int result1 = add1.add();
+        int result2 = add1.subtract();
+
+        if (result1 == a + b){
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Add error!");
+        }
+
+        if (result2 == a - b){
+            System.out.println("Subtract test OK");
+        } else {
+            System.out.println("Subtract error!");
+        }
+
+
+
+
+
+        /*
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+         */
+
     }
 }
