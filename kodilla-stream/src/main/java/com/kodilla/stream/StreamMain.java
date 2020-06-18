bf05f86d846c5653b4c6bcf6dc48e1b8f9895208
Sentarion <mbalabox@gmail.com>
2020-06-18 22:53:58 +0200

    immutable ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -26,5 +27,8 @@ public class StreamMain {
         poemBeautifier.beautify("This is an example text.", text -> text.toUpperCase());
         poemBeautifier.beautify("This is an example text.", text -> "<<<" + text + ">>>");
         poemBeautifier.beautify("This is an example text.", text -> "\"" + text + "\"");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
