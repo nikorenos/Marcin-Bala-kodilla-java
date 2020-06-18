50b3f87db74d802c42bd0f1e271eae2d8c46ee99
Sentarion <mbalabox@gmail.com>
2020-06-18 17:56:12 +0200

    beautify text - lambda

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas:");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references:");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println("Beautify tekst:");
+        poemBeautifier.beautify("This is an example text.", text -> "ABC" + text + "ABC");
+        poemBeautifier.beautify("This is an example text.", text -> text.toUpperCase());
+        poemBeautifier.beautify("This is an example text.", text -> "<<<" + text + ">>>");
+        poemBeautifier.beautify("This is an example text.", text -> "\"" + text + "\"");
+    }
+}
