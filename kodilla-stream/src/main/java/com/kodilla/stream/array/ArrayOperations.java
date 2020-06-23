7f1ccf04baa61924742fc6e2ce8c60ca07547f2b
Sentarion <mbalabox@gmail.com>
2020-06-23 19:35:20 +0200

    average stream fix

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,17 +4,21 @@ import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    static OptionalDouble getAverage(int[] numbers) {
+    static double getAverage(int[] numbers) {
 
-        IntStream marks = IntStream.range(0, numbers.length);
-                marks.forEach(s -> System.out.println(numbers[s]));
-        //Arrays.stream(numbers)
-                //.forEach(s -> System.out.println(s));
+        if (numbers.length > 0) {
+            IntStream marks = IntStream.range(0, numbers.length);
+            marks.forEach(s -> System.out.println(numbers[s]));
+            //Arrays.stream(numbers)
+            //.forEach(s -> System.out.println(s));
 
-        OptionalDouble average = IntStream.range(0, numbers.length)
-                    .map (n -> numbers[n])
-                        .average();
-        return average;
+            OptionalDouble average = IntStream.range(0, numbers.length)
+                    .map(n -> numbers[n])
+                    .average();
+
+            return average.getAsDouble();
+        }
+        return 0;
     }
 
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -5,19 +5,26 @@ import org.junit.Test;
 
 import java.util.OptionalDouble;
 
+import static com.kodilla.stream.array.ArrayOperations.getAverage;
+import static org.junit.Assert.assertEquals;
+
 public class ArrayOperationsTestSuite {
 
     @Test
     public void testGetAverage() {
         //Given
         int[] marks = {5,5,5,4,4,4};
+        int[] marksEmpty = {};
 
         //When
-        OptionalDouble average = ArrayOperations.getAverage(marks);
+        double average = getAverage(marks);
+        double averageZero = getAverage(marksEmpty);
 
         //Then
-        OptionalDouble expectedAverage = OptionalDouble.of(4.5);
-        Assert.assertEquals(expectedAverage, average);
+        double expectedAverage = 4.5;
+        double expectedAverageZero = 0.0;
+        assertEquals(expectedAverage, average, 0.001);
+        assertEquals(expectedAverageZero, averageZero, 0.001);
     }
 
 }
