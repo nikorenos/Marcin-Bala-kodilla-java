600aabefd446decbb66cb6ce7cf6e1f58537cedb
Sentarion <mbalabox@gmail.com>
2020-08-12 17:45:36 +0200

    BoardTestSuite fix

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,6 +1,7 @@
 package com.kodilla.spring.portfolio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -8,10 +9,13 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class BoardConfig {
     @Autowired
+    @Qualifier("toDoList")
     TaskList toDoList;
     @Autowired
+    @Qualifier("inProgressList")
     TaskList inProgressList;
     @Autowired
+    @Qualifier("doneList")
     TaskList doneList;
 
 
